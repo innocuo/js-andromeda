ba54d81dc865f527e94da9a5fca979d603bdbfb6
luis <info@innocuo.com>
2017-05-10 23:37:54 -0500

    update moon version, changed css to flex

--- editor/src/js/components/svg-stage.js
+++ editor/src/js/components/svg-stage.js
@@ -1,5 +1,5 @@
 var Moon = require('moonjs');
 
 Moon.component('svg-stage', {
-  template: "<svg id='main-svg' version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\"></svg>"
+  template: "<div class='svg-container'><svg id='main-svg' version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\"></svg></div>"
 });
