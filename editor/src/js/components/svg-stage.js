9107c385cce57e01315c457553d3f5c6cd823127
luis <info@innocuo.com>
2017-05-12 21:50:47 -0500

    added path drawing via due

--- editor/src/js/components/svg-stage.js
+++ editor/src/js/components/svg-stage.js
@@ -3,7 +3,7 @@ import SVGPath from '../andromeda/svg-path';
 const Snap = require(`imports-loader?this=>window,fix=>module.exports=0!snapsvg/dist/snap.svg.js`);
 
 Vue.component('svg-stage', {
-  template: "<div class='svg-container'><svg id='main-svg' version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\"></svg></div>",
+  template: "<div class='svg-container'><svg id='main-svg' v-on:click=\"click\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\"></svg></div>",
   data: function(){
     const stage_x = 500;
 
@@ -23,6 +23,9 @@ Vue.component('svg-stage', {
       ref: null
     }
   },
+  created: function(){
+    window.addEventListener('mousemove',this.move);
+  },
   mounted: function(){
     this.s = Snap('#main-svg');
     this.s.attr({ viewBox: "0 0 "+this.stage_2x+" "+this.stage_2x , width:"100%", height:"100%"});
@@ -32,6 +35,36 @@ Vue.component('svg-stage', {
     this.ref = this.s.circle(this.stage_x, this.stage_x, this.ref_radius);
     this.ref.attr({
       fill:"#2b96b1"
-    })
+    });
+
+    window.requestAnimationFrame(this.step);
+  },
+  methods:{
+    step: function (timestamp) {
+
+      var ref_w = this.ref.node.getBoundingClientRect().width;
+      this.scale = ref_w/( this.ref_radius * 2 );
+
+      const s_bbox = this.s.node.getBoundingClientRect();
+      var page_width_scaled = s_bbox.width / this.scale;
+      this.mouse.scaledX = this.mouse.x/this.scale - (-this.stage_x + page_width_scaled/2);
+
+      var page_height_scaled = s_bbox.height / this.scale;
+      this.mouse.scaledY = this.mouse.y/this.scale - (-this.stage_x + page_height_scaled/2);
+
+      window.requestAnimationFrame(this.step);
+    },
+    move: function(e){
+      this.mouse.x = e.pageX;
+      this.mouse.y = e.pageY;
+    },
+    click: function(e){
+
+        var path = SVGPath.get(this.s);
+
+        path.add(this.mouse.scaledX, this.mouse.scaledY);
+        path.draw();
+
+    }
   }
 });
